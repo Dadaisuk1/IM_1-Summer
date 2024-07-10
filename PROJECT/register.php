0490723a4ad371854434ac7a42850da48c88a621
IdeapadGaming <largozadarryl9@gmail.com>
2024-07-10 13:58:58 +0800

    move

---
+++ PROJECT/register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Register</title>
+</head>
+<body>
+	<h1>REGISTRATION</h1>
+
+	<h2>User Information</h2>
+
+	<form>
+		<label for="username">User Name</label>
+		<input type="text" name="username" id="username" required>
+		<br>
+		<label for="emailaddress">Email Address</label>
+		<input type="text" name="emailaddress" id="emailaddress" required>
+		<br>
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password" required>
+		<br>
+		<button type="submit">Submit</button>
+	</form>
+</body>
+</html>
