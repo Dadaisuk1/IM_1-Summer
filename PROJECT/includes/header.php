363b36954c114752782ba2766591365d64719f37
IdeapadGaming <largozadarryl9@gmail.com>
2024-07-10 14:53:06 +0800

    header changes

--- PROJECT/includes/header.php
+++ PROJECT/includes/header.php
@@ -17,18 +17,18 @@
 
         <div class="head_nav">
             <a href="home.php">Home</a>
-            <a href="#">About Us</a>
+            <a href="#">Trainor</a>
             <a href="#">Membership</a>
             <a href="#">Contact</a>
-            <a href="#">Trainor</a>
-        </div>
-
-        <div class="navigation_button">
-            <div class="login_button">
-                <a href="login.php">Login</a>
-            </div>
-            <div class="register_button">
-                <a href="register.php">Register</a>
+            <a href="#">About Us</a>
+    
+            <div class="navigation_button">
+                <div class="login_button">
+                    <a href="login.php">Login</a>
+                </div>
+                <div class="register_button">
+                    <a href="register.php">Register</a>
+                </div>
             </div>
         </div>
     </div>
